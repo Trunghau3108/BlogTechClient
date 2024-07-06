26ed5da0c8ed7f81b33e6e8e738c3c970370d246
trunghau3108 <122194066+Trunghau3108@users.noreply.github.com>
2024-07-06 15:37:28 +0700

    HomeGlobal - js - add: Thêm api và binding lên trang Home

--- src/app/bt-app/bt-home/pages/home-global/home-global.component.ts
+++ src/app/bt-app/bt-home/pages/home-global/home-global.component.ts
@@ -7,8 +7,11 @@ import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
 import { IconsModule } from '@progress/kendo-angular-icons';
 import { searchIcon,homeIcon,myspaceIcon,thumbUpOutlineIcon, chevronRightIcon } from '@progress/kendo-svg-icons';
-import { Router, RouterModule } from "@angular/router";
+import { RouterModule } from "@angular/router";
 import { NgFor } from '@angular/common';
+import { DTOBlog } from '../../shared/dto/DTOBlog.dto';
+import { BlogApiService } from '../../shared/services/blog-api.service';
+import { Ps_UtilObjectService } from '../../../../bt-lib/utilities/utility.object';
 @Component({
   selector: 'app-home-global',
   standalone: true,
@@ -28,6 +31,8 @@ export class HomeGlobalComponent {
   icons = { searchIcon: searchIcon ,myspace:myspaceIcon,home:homeIcon,thumb: thumbUpOutlineIcon,right: chevronRightIcon};
   data = [{code:1,name:"Test"}]
 
+  constructor(public blogAPIService: BlogApiService){}
+
   items = [
     { title: 'Item 1', image: 'bookInHand.jpg', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
     { title: 'Item 2', image: 'bookInHand.jpg', content: 'Suspendisse varius enim in eros.' },
@@ -36,5 +41,31 @@ export class HomeGlobalComponent {
     { title: 'Item 5', image: 'bookInHand.jpg', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
     { title: 'Item 6', image: 'bookInHand.jpg', content: 'Suspendisse varius enim in eros.' }
   ];
+
+  ngOnInit():void{
+    this.GetAllBlog()
+  }
+
+  listBlog: DTOBlog[] = []
+  listTrending: DTOBlog[] = []
+  listFeatured: DTOBlog[] = []
+  listPopular: DTOBlog[] = []
+  GetAllBlog() {
+    // this.isLoading = true
+    this.blogAPIService.GetAllBlog().subscribe((response: any) => {
+      if(Ps_UtilObjectService.hasListValue(response)){
+        this.listBlog = response
+        // console.log(this.listBlog)
+        this.listTrending = this.listBlog.filter(s => s.isTrending == true)
+        this.listFeatured = this.listBlog.filter(s => s.isFeatured == true)
+        this.listPopular = this.listBlog.filter(s => s.isPopular == true)
+      }
+      // this.isLoading = false
+    }, (error: any) => {
+      // this.isLoading = false
+      console.error(error);
+      alert(error.message + `:  ${error.error.message}` )
+    });
+  }
   
 }
